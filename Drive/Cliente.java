60db574e029251bff79651a2f4c3209eea44a1ce
Leonardo Freitas <leoneryfreitas@gmail.com>
2025-03-29 10:45:50 -0300

    Conexão Cliente

--- Drive/Cliente.java
+++ Drive/Cliente.java
@@ -1,4 +1,21 @@
 package Drive;
 
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
 public class Cliente {
-}
+    public static void main(String[] args) throws IOException {
+
+        Socket socket = new Socket("Localhost" , 12345);
+        Scanner sc = new Scanner(System.in);
+
+        ClienteThread clienteThread = new ClienteThread(socket);
+        clienteThread.start();
+        //Classe para enviar informações para o servidor
+        PrintStream saida = new PrintStream(socket.getOutputStream());
+        String teclado = sc.nextLine();
+        saida.println(teclado);
+        }
+    }
+
