3e2757e21562f9db6c3f63aac139ddd6af79b772
Leonardo Freitas <leoneryfreitas@gmail.com>
2025-03-29 22:32:40 -0300

    Cliente comentado

--- Drive/Cliente.java
+++ Drive/Cliente.java
@@ -7,9 +7,11 @@ import java.util.Scanner;
 public class Cliente {
     public static void main(String[] args) throws IOException {
 
+        //Estabelece uma conexão com o servidor
         Socket socket = new Socket("Localhost" , 12345);
         Scanner sc = new Scanner(System.in);
 
+        //Rersponsaveis por permitir que o cliente receba e envie mensagem ao servidor
         ClienteThread clienteThread = new ClienteThread(socket);
         clienteThread.start();
         //Classe para enviar informações para o servidor
